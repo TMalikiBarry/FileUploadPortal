8fddaac28a652bd15cbd715e228705f4954b056e
babacar.adje <babacar.adje@intouchgroup.net>
2023-11-21 10:56:29 +0000

    revue environment.ts index.html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // API_URL: "http://localhost:2020/api/v1",
   //API_URL: "https://dev-touch-ssii-api.gutouch.net/gateway/api/v1",
-  API_URL: "https://prod-ssii.gutouch.net/gateway/api/v1",
+  API_URL: "https://ecobank.gutouch.net/gateway/api/v1",
 
 };
